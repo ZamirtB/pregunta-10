49dddc0de5185324c1112c59f32f956578f29c66
zamirt barrera <61028965+ZamirtB@users.noreply.github.com>
2020-04-19 00:11:28 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.textEntrada = new System.Windows.Forms.TextBox();
+            this.digitoResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(64, 92);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Multiplo";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(161, 92);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "divisores";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(64, 144);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "Factorial";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(161, 144);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "fibonacci";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // textEntrada
+            // 
+            this.textEntrada.Location = new System.Drawing.Point(29, 55);
+            this.textEntrada.Name = "textEntrada";
+            this.textEntrada.Size = new System.Drawing.Size(251, 20);
+            this.textEntrada.TabIndex = 14;
+            this.textEntrada.TextChanged += new System.EventHandler(this.digitoRecibido_TextChanged);
+            // 
+            // digitoResultado
+            // 
+            this.digitoResultado.Location = new System.Drawing.Point(29, 227);
+            this.digitoResultado.Name = "digitoResultado";
+            this.digitoResultado.Size = new System.Drawing.Size(251, 20);
+            this.digitoResultado.TabIndex = 15;
+            this.digitoResultado.TextChanged += new System.EventHandler(this.digitoResultado_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Digite el numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "resultado";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.digitoResultado);
+            this.Controls.Add(this.textEntrada);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.TextBox textEntrada;
+        private System.Windows.Forms.TextBox digitoResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+
+    {
+        private int z;
+        private int result;
+        private String text;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            z = int.Parse(textEntrada.Text);
+            for (int i = 0; i <= 9; i++)
+            {
+                result = z * i;
+                text = text + result.ToString() + "  ";
+                digitoResultado.Text = text;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            z = int.Parse(textEntrada.Text);
+            for (int i = 1; i <= z; i++)
+            {
+                double b = Math.IEEERemainder(z, i);
+                if (b == 0)
+                {
+                    text = text + i.ToString() + "  ";
+                    digitoResultado.Text = text;
+                }
+            }
+        }
+
+        private void digitoRecibido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void digitoResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            result = 1;
+            z = int.Parse(textEntrada.Text);
+            for (int i = 1; i <= z; i++)
+            {
+                result = result * i;
+            }
+            text = text + result.ToString();
+            digitoResultado.Text = text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            result = 0;
+            int b = 1;
+            int z = 0;
+            this.z = int.Parse(textEntrada.Text);
+            text = text + b.ToString() + "  ";
+            digitoResultado.Text = text;
+            for (int i = 1; i < this.z; i++)
+            {
+                z = result;
+                result = b;
+                b = z + result;
+                text = text + b.ToString() + "  ";
+                digitoResultado.Text = text;
+            }
+        }
+    }
+}
